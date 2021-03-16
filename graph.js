4f0aa4e505460574eb0c11f298c122a170d10468
Bob German <bgerman1@hotmail.com>
2021-03-16 13:01:43 -0400

    Added changes for file upload/download

--- graph.js
+++ graph.js
@@ -16,3 +16,56 @@ async function getUser() {
         .get();
 }
 
+//#region Add for file download
+
+    // Get files in root of user's OneDrive
+    async function getFiles() {
+        ensureScope('files.read');
+        try {
+            const response = await graphClient
+                .api('/me/drive/root/children')
+                .select('id,name,folder,package')
+                .get();
+            return response.value;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function downloadFile(file) {
+        try {
+            const response = await graphClient
+                .api(`/me/drive/items/${file.id}`)
+                .select('@microsoft.graph.downloadUrl')
+                .get();
+            const downloadUrl = response["@microsoft.graph.downloadUrl"];
+            window.open(downloadUrl, "_self");
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+//#endregion
+
+//#region  Add for file upload
+
+    // Upload file to OneDrive
+    async function uploadFile(file) {
+        try {
+            ensureScope('files.readwrite');
+            let options = {
+                path: "/",
+                fileName: file.name,
+                rangeSize: 1024 * 1024 // must be a multiple of 320 KiB
+            };
+            const uploadTask = 
+                await MicrosoftGraph.OneDriveLargeFileUploadTask.create(graphClient, file, options);
+            const response = await uploadTask.upload();
+            console.log(`File ${response.name} of ${response.size} bytes uploaded`);
+            return response;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+//#endregion
--- ui.js
+++ ui.js
@@ -11,4 +11,48 @@ async function displayUI() {
     signInButton.style = "display: none";
     var content = document.getElementById('content');
     content.style = "display: block";
+
+    displayFiles();
 }
+
+//#region Add for file download
+
+async function displayFiles() {
+    
+    const files = await getFiles();
+    const ul = document.getElementById('downloadLinks');
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+    for (let file of files) {
+        if (!file.folder && !file.package) {
+            let a = document.createElement('a');
+            a.href = '#';
+            a.onclick = () => { downloadFile(file); };
+            a.appendChild(document.createTextNode(file.name));
+            let li = document.createElement('li');
+            li.appendChild(a);
+            ul.appendChild(li);
+        }
+    }
+}
+
+//#endregion
+
+//#region Add for file upload
+
+function fileSelected(e) {
+    displayUploadMessage(`Uploading ${e.files[0].name}...`);
+    uploadFile(e.files[0])
+    .then((response) => {
+      displayUploadMessage(`File ${response.name} of ${response.size} bytes uploaded`);
+      displayFiles();
+    });
+}
+
+function displayUploadMessage(message) {
+    const messageElement = document.getElementById('uploadMessage');
+    messageElement.innerText = message;
+}
+
+//#endregion
