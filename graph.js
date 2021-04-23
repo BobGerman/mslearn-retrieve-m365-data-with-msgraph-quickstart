84f3f90d93b51383763d082471eee101e8d60b87
Bob German <bgerman1@hotmail.com>
2021-04-23 15:05:41 -0400

    UI now works

--- graph.js
+++ graph.js
@@ -18,11 +18,6 @@ async function getUser() {
 
 //#region Excel scenario
 
-// https://graph.microsoft.com/v1.0/me/drive/root:/Bacon Survey.xlsx:/workbook/
-// https://graph.microsoft.com/v1.0/me/drive/root:/Bacon Survey.xlsx:/workbook/worksheets/Bacon
-// https://graph.microsoft.com/v1.0/me/drive/root:/Bacon Survey.xlsx:/workbook/worksheets/Bacon/Charts/Chart 1
-// https://graph.microsoft.com/v1.0/me/drive/root:/Bacon Survey.xlsx:/workbook/worksheets/Bacon/Charts/Chart 1/image
-
 async function getChartImage(filePath, worksheetName, chartName) {
     ensureScope('files.read');
     try {
@@ -34,8 +29,6 @@ async function getChartImage(filePath, worksheetName, chartName) {
         console.error(error);
     }
 }
-
-// https://graph.microsoft.com/v1.0/me/drive/root:/Bacon Survey.xlsx:/workbook/worksheets/Bacon/Tables/Responses/rows
 
 async function addSurveyResult(filePath, worksheetName, tableName, person, choice) {
     ensureScope('files.readwrite');
@@ -51,62 +44,5 @@ async function addSurveyResult(filePath, worksheetName, tableName, person, choic
         console.error(error);
     }
 }
-
-//#endregion
-
-
-
-//#region Add for file download
-
-    // Get files in root of user's OneDrive
-    async function getFiles(filename) {
-        ensureScope('files.read');
-        try {
-            const response = await graphClient
-                .api("/me/drive/root/children")
-                .select("id,name,folder,package")
-                .filter(`name eq '${filename}'`)
-                .get();
-            return response.value;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function downloadFile(file) {
-        try {
-            const response = await graphClient
-                .api(`/me/drive/items/${file.id}`)
-                .select('@microsoft.graph.downloadUrl')
-                .get();
-            const downloadUrl = response["@microsoft.graph.downloadUrl"];
-            window.open(downloadUrl, "_self");
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-//#endregion
-
-//#region  Add for file upload
-
-    // Upload file to OneDrive
-    async function uploadFile(file) {
-        try {
-            ensureScope('files.readwrite');
-            let options = {
-                path: "/",
-                fileName: file.name,
-                rangeSize: 1024 * 1024 // must be a multiple of 320 KiB
-            };
-            const uploadTask = 
-                await MicrosoftGraph.OneDriveLargeFileUploadTask.create(graphClient, file, options);
-            const response = await uploadTask.upload();
-            console.log(`File ${response.name} of ${response.size} bytes uploaded`);
-            return response;
-        } catch (error) {
-            console.error(error);
-        }
-    }
 
 //#endregion
--- ui.js
+++ ui.js
@@ -12,52 +12,40 @@ async function displayUI() {
     var content = document.getElementById('content');
     content.style = "display: block";
 
-    displayFiles();
+    displaySurvey(user.displayName);
 }
 
-//#region Add for file download
+//#region Add for excel
 
-async function displayFiles() {
+function displaySurvey(userName) {
 
-    await addSurveyResult ("/samples/Bacon Survey.xlsx", "Bacon", "Responses", "Bob", "Irish");
+    const surveyButton = document.getElementById('surveyButton');
+    surveyButton.onclick = async () => {
+    
+        // Find the radio button that was selected
+        const radioButtons = Array.from(document.querySelectorAll('input[name="choice"]'));
+        let selectedValue = radioButtons.find(rb => rb.checked)?.value;
+
+        // Add the survey response and display the overall results
+        await addSurveyResult ("/samples/Bacon Survey.xlsx", "Bacon", "Responses", userName, selectedValue);
+        await displayResults();
+    
+    }
+    
+}
+
+async function displayResults() {
+
+    // Get the chart in Base 65 and display it in the image element
     const chartImage = await getChartImage("/samples/Bacon Survey.xlsx", "Bacon", "Chart 1");
     const chartElement = document.getElementById('resultsChart');
     chartElement.src=`data:image/png;base64,${chartImage}`;
 
-    const files = await getFiles();
-    const ul = document.getElementById('downloadLinks');
-    while (ul.firstChild) {
-        ul.removeChild(ul.firstChild);
-    }
-    for (let file of files) {
-        if (!file.folder && !file.package) {
-            let a = document.createElement('a');
-            a.href = '#';
-            a.onclick = () => { downloadFile(file); };
-            a.appendChild(document.createTextNode(file.name));
-            let li = document.createElement('li');
-            li.appendChild(a);
-            ul.appendChild(li);
-        }
-    }
+    // Hide the form, show the results
+    document.getElementById("surveyForm").style = "display:none";
+    document.getElementById("surveyResults").style = "display:block";
+    
 }
 
-//#endregion
-
-//#region Add for file upload
-
-function fileSelected(e) {
-    displayUploadMessage(`Uploading ${e.files[0].name}...`);
-    uploadFile(e.files[0])
-    .then((response) => {
-      displayUploadMessage(`File ${response.name} of ${response.size} bytes uploaded`);
-      displayFiles();
-    });
-}
-
-function displayUploadMessage(message) {
-    const messageElement = document.getElementById('uploadMessage');
-    messageElement.innerText = message;
-}
 
 //#endregion
