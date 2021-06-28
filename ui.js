3be4b86202071b7e7b1d558e7baedbadbf0be3db
Bob German <bgerman1@hotmail.com>
2021-06-28 13:46:27 -0400

    Broke out steps 1 and 2 properly

--- ui.js
+++ ui.js
@@ -31,16 +31,24 @@ document.getElementById('insightsButton').onclick =
             if (files.length === 0) {
                 resultDiv.innerHTML = `<p>No ${insightType} files found</p>`;
             } else {
+                // STEP 1
                 resultDiv.innerHTML = `
                     <h3>${insightName} files for ${upn ? upn : "/me"}:</h3>
                     <ul>
                         ${files.map(file => `<li>
-                        <a href="${file.webUrl}" target="_blank">${file.name}</a><br />
-                        <i><small>Last modified by ${file.lastModifiedBy.user.email} 
-                            on ${file.lastModifiedDateTime}</small></i>
-                        </li>`)}
-                    </ul>
-                    `;
+                        <a href="${file.webUrl}" target="_blank">${file.name}</a>
+                        </li>`).join('')}
+                    </ul>`;
+                // STEP 2
+                // resultDiv.innerHTML = `
+                //     <h3>${insightName} files for ${upn ? upn : "/me"}:</h3>
+                //     <ul>
+                //         ${files.map(file => `<li>
+                //         <a href="${file.webUrl}" target="_blank">${file.name}</a><br />
+                //         <i><small>Last modified by ${file.lastModifiedBy.user.email} 
+                //             on ${file.lastModifiedDateTime}</small></i>
+                //         </li>`).join('')}
+                //     </ul>`;
             }
         } catch (error) {
             const message = insightType === 'shared' ? "NOTE: Users can only read their own shared insights" : "";
